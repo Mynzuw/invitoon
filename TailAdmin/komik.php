3f4aa2a21b3ac50949c9c9f90c07e6ec564017d1
Miftah Adha <miftahadha.ma@gmail.com>
2023-12-10 20:04:58 +0700

    commit

--- TailAdmin/komik.php
+++ TailAdmin/komik.php
@@ -190,8 +190,96 @@ include("../koneksi.php");
         
 
 
-        
-        <!-- Main modal -->
+
+        <form action="proses_edit.php" method="POST" >
+<div id="crud-modal-edit<?= $row["komik_id"] ?>" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative p-4 w-full max-w-md max-h-full">
+        <!-- Modal content -->
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <!-- Modal header -->
+            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                    Edit Komik
+                </h3>
+                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="crud-modal-edit<?= $row["komik_id"] ?>">
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                    </svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+            </div>
+            <!-- Modal body -->
+            <form class="p-4 md:p-5" method="POST" action="proses_edit.php" enctype="multipart/form-data">
+                <div class="grid gap-4 mb-4 grid-cols-2">
+                <input name="komik_id" value="<?php echo $row['komik_id']; ?>"  hidden />
+                    <div class="col-span-2">
+                        <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Judul Komik</label>
+                        <input type="text" name="judul_komik" id="judul_komik" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Tulis judul komik" value="<?= $row["judul_komik"] ?>" required="">
+                    </div>
+                    <div class="col-span-2">
+                        <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Deskripsi Komik</label>
+                        <textarea id="deskripsi" name="deskripsi" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Tulis deskripsi komik disini"><?php echo $row["deskripsi"]; ?></textarea>                    
+                    </div>
+                    <div class="col-span-2 sm:col-span-1">
+                        <label for="penulis" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Penulis</label>
+                        <input type="text" name="pengarang" id="penulis" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Nama penulis" value="<?= $row["pengarang"] ?>" required="">
+                    </div>
+                    <div class="col-span-2 sm:col-span-1">
+                        <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Genre</label>
+                        <select id="category" name="genre" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                            <option selected=""><?= $row["genre"] ?></option>
+                            <option value="Fantasi">Fantasi</option>
+                            <option value="Aksi">Aksi</option>
+                            <option value="Misteri">Misteri</option>
+                            <option value="Romantis">Romantis</option>
+                            <option value="Komedi">Komedi</option>
+                            <option value="Horor">Horor</option>
+                        </select>
+                    </div>
+
+                    <div class="col-span-2">
+                    <span class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Cover Komik</span>
+                            <div class="flex justify-center items-center w-full">
+                                <label for="dropzone-file" class="flex flex-col justify-center items-center w-full h-64 bg-gray-50 rounded-lg border-2 border-gray-300 border-dashed cursor-pointer dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                                <div class="preview">
+                                <img src="../admin/gambar/<?php echo $row['cover']; ?>" style="width: 120px;float: left;margin-bottom: 5px;">
+                                    <img id="file-ip-1-preview" style ="width:100px">
+                                </div>                                
+                                <input type="file" name="cover" id="file-ip-1-preview" onchange="showPreview(event)";/>
+                                </label>
+                            </div>
+                        </div>
+                    
+                </div>
+                <button type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
+                    Edit komik
+                </button>
+            </form>
+        </div>
+    </div>
+</div>
+</form>
+       
+
+<button class="edit-button text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" data-modal-target="crud-modal-edit<?= $row["komik_id"] ?>" data-modal-toggle="crud-modal-edit<?= $row["komik_id"] ?>">Edit</button>
+        <a class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" href="proses_hapus.php?komik_id=<?= $row["komik_id"] ?>" onclick="return confirm('Anda yakin akan menghapus data ini?')">Hapus</a>
+        </td>
+      </tr>
+<?php
+ }
+}
+mysqli_close($conn);
+?>
+    </tbody>
+         
+    </table>
+</div>
+</div>
+
+
+<!-- Modal Edit -->
+ <!-- Main modal -->
 <form action="proses_edit.php" method="POST" >
 <div id="crud-modal-edit<?= $row["komik_id"] ?>" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
     <div class="relative p-4 w-full max-w-md max-h-full">
@@ -260,24 +348,7 @@ include("../koneksi.php");
         </div>
     </div>
 </div>
-</form><button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" data-modal-target="crud-modal-edit<?= $row["komik_id"] ?>" data-modal-toggle="crud-modal-edit<?= $row["komik_id"] ?>">Edit</button>
-        <a class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" href="proses_hapus.php?komik_id=<?= $row["komik_id"] ?>" onclick="return confirm('Anda yakin akan menghapus data ini?')">Hapus</a>
-        </td>
-      </tr>
-<?php
- }
-}
-mysqli_close($conn);
-?>
-    </tbody>
-         
-    </table>
-</div>
-</div>
-
-
-<!-- Modal Edit -->
-
+</form>
 
 
 
@@ -307,7 +378,9 @@ mysqli_close($conn);
     preview.style.display = "block";
   }
 }
+
 </script>
+
 
         
 </body>
--- invitoon.sql
+++ invitoon.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Waktu pembuatan: 08 Des 2023 pada 08.07
+-- Waktu pembuatan: 09 Des 2023 pada 08.23
 -- Versi server: 5.7.24
 -- Versi PHP: 8.1.10
 
